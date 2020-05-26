5124f89a24a69cff8db819434f629a8b3b3849c3
Odysseus Chiu <odysseus@highwaythreesolutions.com>
2020-05-26 12:22:09 -0700

    3765 - folio number

--- src/registry_schemas/version.py
+++ src/registry_schemas/version.py
@@ -22,4 +22,4 @@ Post-release segment: .postN
 Development release segment: .devN
 """
 
-__version__ = '2.5.11'  # pylint: disable=invalid-name
+__version__ = '2.5.12'  # pylint: disable=invalid-name
