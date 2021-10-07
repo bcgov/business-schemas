282355c5affd92a4cd5c8910a6bb34ee5fc15c5e
vysakh.menon <vysakh.menon@aot-technologies.com>
2021-10-07 06:52:25 -0700

    9055 courtOrder in dissolution

--- tests/unit/test_dissolution.py
+++ tests/unit/test_dissolution.py
@@ -14,6 +14,8 @@
 """Test Suite to ensure dissolution schemas are valid."""
 import copy
 
+import pytest
+
 from registry_schemas import validate
 from registry_schemas.example_data import DISSOLUTION, FILING_HEADER
 
@@ -62,3 +64,59 @@ def test_voluntary_dissolution_schema():
     print(errors)
 
     assert is_valid
+
+
+def test_dissolution_court_order():
+    """Assert a valid court orders."""
+    legal_filing = {'dissolution': copy.deepcopy(DISSOLUTION)}
+    legal_filing['dissolution']['courtOrder'] = {
+        'fileNumber': '12345',
+        'effectOfOrder': 'planOfArrangement'
+    }
+
+    is_valid, errors = validate(legal_filing, 'dissolution')
+
+    if errors:
+        for err in errors:
+            print(err.message)
+    print(errors)
+
+    assert is_valid
+
+
+@pytest.mark.parametrize('invalid_court_order', [
+    *[{'orderDate': '2021-01-30T09:56:01+08:00',
+       'effectOfOrder': 'valid effect of order'}],
+    *[{
+        'fileNumber': invalid_file_number,
+        'orderDate': '2021-01-30T09:56:01+08:00'
+    } for invalid_file_number in ['1234', '123456789012345678901']],
+    *[{
+        'fileNumber': '12345',
+        'orderDate': invalid_order_date
+    } for invalid_order_date in ['2021-01-30T09:56:01', '2021-01-30']],
+    *[{
+        'fileNumber': '12345',
+        'orderDate': '2021-01-30T09:56:01+08:00',
+        'effectOfOrder': invalid_effect_of_order
+    } for invalid_effect_of_order in [('a' * 501)]],  # long effectOfOrder
+    *[{
+        'fileNumber': '12345',
+        'orderDate': '2021-01-30T09:56:01+08:00',
+        'effectOfOrder': 'planOfArrangement',
+        'orderDetails': invalid_order_details
+    } for invalid_order_details in [('a' * 2001)]],  # long orderDetails
+])
+def test_validate_invalid_court_orders(invalid_court_order):
+    """Assert not valid court orders."""
+    legal_filing = {'dissolution': copy.deepcopy(DISSOLUTION)}
+    legal_filing['dissolution']['courtOrder'] = invalid_court_order
+
+    is_valid, errors = validate(legal_filing, 'dissolution')
+
+    if errors:
+        for err in errors:
+            print(err.message)
+    print(errors)
+
+    assert not is_valid
