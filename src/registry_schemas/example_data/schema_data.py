be14aed9f0388ba37fe2ce1006282098f4bfdd09
Lekshmi Mallika <lekshmi.mallika@aot-technologies.com>
2021-12-07 15:28:01 -0800

    Business schema update for entity state

--- src/registry_schemas/example_data/schema_data.py
+++ src/registry_schemas/example_data/schema_data.py
@@ -49,7 +49,34 @@ BUSINESS = {
     'lastLedgerTimestamp': '2019-04-15T20:05:49.068272+00:00',
     'lastPreBobFilingTimestamp': '2019-04-15T20:05:49.068272+00:00',
     'legalName': 'legal name - CP1234567',
-    'legalType': 'CP'
+    'legalType': 'CP',
+    'state': {
+        'state': 'ACTIVE',
+        'reason': 'Active'
+    },
+    'goodStanding': True,
+    'complianceWarnings': [
+        {
+            'code': 'INVALID_LEGAL_STRUCTURE_DIRECTORS',
+            'message': 'A minimum of 3 directors is required.',
+            'filing': 'https://LEGAL-API-HOST/api/v2/businesses/IDENTIFIER/filings/FILING_ID'
+        }
+    ]
+}
+
+BUSINESS_HISTORICAL = {
+    'cacheId': 1,
+    'foundingDate': '2007-04-08T00:00:00+00:00',
+    'identifier': 'CP1234567',
+    'lastLedgerTimestamp': '2019-04-15T20:05:49.068272+00:00',
+    'lastPreBobFilingTimestamp': '2019-04-15T20:05:49.068272+00:00',
+    'legalName': 'legal name - CP1234567',
+    'legalType': 'CP',
+    'state': {
+        'state': 'HISTORICAL',
+        'reason': 'Voluntary Dissolution',
+        'filing': 'https://LEGAL-API-HOST/api/v2/businesses/IDENTIFIER/filings/FILING_ID'
+    }
 }
 
 ADDRESS = {
