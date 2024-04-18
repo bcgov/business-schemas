69723b20f78ac15b108bbccda951e191a3abe51e
Kevin Zhang <kevinzhang940420@gmail.com>
2024-04-18 09:01:56 -0700

    20766 add alternate_name

--- src/registry_schemas/example_data/schema_data.py
+++ src/registry_schemas/example_data/schema_data.py
@@ -37,6 +37,7 @@ FILING_HEADER = {
             'lastLedgerTimestamp': '2019-04-15T20:05:49.068272+00:00',
             'lastPreBobFilingTimestamp': '2019-04-15T20:05:49.068272+00:00',
             'legalName': 'legal name - CP1234567',
+            'alternateNames': [],
             'legalType': 'CP'
         }
     }
@@ -49,6 +50,7 @@ BUSINESS = {
     'lastLedgerTimestamp': '2019-04-15T20:05:49.068272+00:00',
     'lastPreBobFilingTimestamp': '2019-04-15T20:05:49.068272+00:00',
     'legalName': 'legal name - CP1234567',
+    'alternateNames': [],
     'legalType': 'CP',
     'state': 'ACTIVE',
     'goodStanding': True,
